0b272a5d2ff5e807bd739fcfda867f079608215c
JacMatBue <jacques.buesche@gmail.com>
2023-06-25 18:56:55 +0200

    Fehlerbehebung

--- Bulgaria.R
+++ Bulgaria.R
@@ -54,8 +54,10 @@ MP_Bulgaria_1998 <- MP_Data %>%
 DPI_Bulgaria_1998 <- DPI_Data %>%
   subset(countryname == "Bulgaria") %>%
   mutate(year = substr(year, 1, 4)) %>%
-  subset(as.numeric(substr(year, 1, 4)) >= 1998)
+  subset(as.numeric(substr(year, 1, 4)) >= 1998) %>%
   
+  mutate(year = as.numeric(year), year_minus_1 = year - 1) %>% # das DPI Datenset ist um eins verschoben -> MP: Juni 2000 Wahl, DPI zhält das angebrochene Jahr noch zur vorherigen Regierung
+  mutate(year_minus_1 = as.character(year_minus_1))
   
 DPI_Bulgaria_1998_test <- DPI_Bulgaria_1998 %>%
   mutate(
@@ -70,13 +72,13 @@ DPI_Bulgaria_1998_test <- DPI_Bulgaria_1998 %>%
 # zur besseren visualisierung von DPI_Bulgaria
 
 DPI_BGR_visual <- DPI_Bulgaria_1998 %>%
-  subset(select = c(year, gov1me, gov1vote, gov2me, gov2vote, gov3me, gov3vote, opp1me, opp1vote, opp2me, opp2vote, opp3me, opp3vote))
+  subset(select = c(year, year_minus_1, gov1me, gov1vote, gov2me, gov2vote, gov3me, gov3vote, opp1me, opp1vote, opp2me, opp2vote, opp3me, opp3vote))
   
   
 ## Variable government (0/1) in MP_Bulgaria_1998 hinzufügen
 
 MP_DPI_Bulgaria_1998 <- MP_Bulgaria_1998 %>%
-  left_join(select(DPI_Bulgaria_1998_test, year, gov1me, gov2me, gov3me), by = c("year" = "year")) %>% ## weil DPI zu groß nur bestimmte Variablen von Interesse joinen
+  left_join(select(DPI_Bulgaria_1998_test, year, year_minus_1, gov1me, gov2me, gov3me), by = c("year" = "year_minus_1")) %>% ## weil DPI zu groß nur bestimmte Variablen von Interesse joinen
   mutate(government = case_when(
     partyabbrev == gov1me | partyabbrev == gov2me | partyabbrev == gov3me ~ 1,
     TRUE ~ 0
@@ -96,12 +98,21 @@ BGR_1998 <- MP_DPI_Bulgaria_1998 %>%
 # Variable "Wahlkosten" erstellen
 
 BGR_1998_X1 <- BGR_1998 %>%
-  arrange(partyname, year) %>%
   group_by(partyname) %>%
   mutate(Wahlkosten = pervote - lag(pervote)) %>%
   ungroup()
 
+## Welche Fälle waren in der Regierung und sind populistisch
 
+populistisch_regierung <- BGR_1998_X1 %>%
+  subset(government == 1 & populist == 1)
+
+
+## Regressionsanalyse
+
+regression_model1 <- lm(Wahlkosten ~ government, data = BGR_1998_X1)
+
+summary(regression_model1)
 
 ## Datenset muss nochmal überprüft und nachgebessert werden: 2017/18 war z.B. OP in Regierung
   
